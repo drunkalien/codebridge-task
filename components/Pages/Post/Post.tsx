d5e3c9f9139bea8053cbd2894eb7e3d86784886b
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-11-24 12:17:23 +0500

    moved dynamic prop to component

--- components/Pages/Post/Post.tsx
+++ components/Pages/Post/Post.tsx
@@ -3,18 +3,19 @@ import Link from "next/link";
 import { Container, Link as MuiLink } from "@mui/material";
 
 import { selectPosts } from "../../../features/posts/postsSlice";
+import { useRouter } from "next/router";
 
-type Props = {
-  id: number;
-};
-
-const Post = ({ id }: Props) => {
+const Post = () => {
   const posts = useSelector(selectPosts);
+  const router = useRouter();
+  const { id } = router.query;
+  const idx = parseInt(id as string);
+
   return (
     <Container>
-      <h2>{posts?.articles[id]?.title}</h2>
-      <strong>{posts?.articles[id]?.description}</strong>
-      <p>{posts?.articles[id]?.content}</p>
+      <h2>{posts?.articles[idx]?.title}</h2>
+      <strong>{posts?.articles[idx]?.description}</strong>
+      <p>{posts?.articles[idx]?.content}</p>
       <Link href="/" passHref>
         <MuiLink color="#2832c2" underline="none">
           {"<"} Go Back
--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -2,9 +2,7 @@ import { useRouter } from "next/dist/client/router";
 import { Post } from "../../components";
 
 const PostPage = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  return id && <Post id={parseInt(id as string)} />;
+  return <Post />;
 };
 
 export default PostPage;
